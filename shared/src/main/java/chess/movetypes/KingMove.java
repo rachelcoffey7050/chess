9160be27b496f141aa575b22aa918143afdeb0be
Rachel Coffey <rachelc7050@gmail.com>
2026-01-20 14:05:37 -0700

    Implemented KingMove, untested

--- shared/src/main/java/chess/movetypes/KingMove.java
+++ shared/src/main/java/chess/movetypes/KingMove.java
@@ -1,4 +1,38 @@
 package chess.movetypes;
 
+import chess.ChessBoard;
+import chess.ChessMove;
+import chess.ChessPiece;
+import chess.ChessPosition;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
 public class KingMove {
+    public Collection<ChessMove> moves;
+
+    public   KingMove (ChessBoard board, ChessPosition position) {
+        this.moves = new ArrayList<ChessMove>();
+        ArrayList<ChessPosition> positions = new ArrayList<ChessPosition>();
+        positions.add(new ChessPosition(position.getRow()-1, position.getColumn()));
+        positions.add(new ChessPosition(position.getRow()+1, position.getColumn()));
+        positions.add(new ChessPosition(position.getRow(), position.getColumn()-1));
+        positions.add(new ChessPosition(position.getRow(), position.getColumn()+1));
+        for (ChessPosition p: positions) {
+            if (inBounds(p) && board.getPiece(position)==null){
+                moves.add(new ChessMove(position, p, ChessPiece.PieceType.KING));
+            }
+        }
+
+
+    }
+
+    public static boolean inBounds(ChessPosition position) {
+        if (0 < position.getRow() && position.getRow() < 9 && position.getColumn() > 0 && position.getColumn() < 9) {
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
 }
