66d06d17819c9716332a83a35449702619745dbe
Rachel Coffey <rachelc7050@gmail.com>
2026-01-20 20:41:09 -0700

    pawn moves minus promotion

--- shared/src/main/java/chess/movetypes/PawnMove.java
+++ shared/src/main/java/chess/movetypes/PawnMove.java
@@ -1,4 +1,32 @@
 package chess.movetypes;
 
-public class PawnMove {
+import chess.*;
+
+import java.util.ArrayList;
+
+public class PawnMove extends Moves{
+
+    public PawnMove(ChessBoard board, ChessPosition position, ChessGame.TeamColor color) {
+        this.moves = new ArrayList<>();
+
+        int newRow;
+        if (color == ChessGame.TeamColor.WHITE) {
+            newRow = position.getRow()+1;
+        } else {newRow = position.getRow()-1;}
+        //forward
+        ChessMove forward = new ChessMove(position, new ChessPosition(newRow, position.getColumn()), ChessPiece.PieceType.PAWN);
+        if (board.getPiece(forward.getEndPosition())==null && inBounds(forward.getEndPosition())){
+            moves.add(forward);
+        }
+
+        //attack
+        ChessMove attack1 = new ChessMove(position, new ChessPosition(newRow, position.getColumn()-1), ChessPiece.PieceType.PAWN);
+        if (board.getPiece(forward.getEndPosition()).getTeamColor()!=color && inBounds(forward.getEndPosition())){
+            moves.add(attack1);
+        }
+        ChessMove attack2 = new ChessMove(position, new ChessPosition(newRow, position.getColumn()+1), ChessPiece.PieceType.PAWN);
+        if (board.getPiece(forward.getEndPosition()).getTeamColor()!=color && inBounds(forward.getEndPosition())){
+            moves.add(attack2);
+        }
+    }
 }
