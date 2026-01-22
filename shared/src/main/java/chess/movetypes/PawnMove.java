8650d44bb972b6e39c309bb59948fec704197f9c
Rachel Coffey <rachelc7050@gmail.com>
2026-01-21 18:01:03 -0700

    hopefully fixed pawn

--- shared/src/main/java/chess/movetypes/PawnMove.java
+++ shared/src/main/java/chess/movetypes/PawnMove.java
@@ -15,25 +15,40 @@ public class PawnMove extends Moves{
         } else {newRow = position.getRow()-1;}
 
         //promotion
-        ChessPiece.PieceType type;
+        ArrayList<ChessPiece.PieceType> types = new ArrayList<>();
         if (newRow==1 || newRow==8) {
-            type = ChessPiece.PieceType.QUEEN;
-        } else {type = ChessPiece.PieceType.PAWN;}
+            types.add(ChessPiece.PieceType.QUEEN);
+            types.add(ChessPiece.PieceType.BISHOP);
+            types.add(ChessPiece.PieceType.ROOK);
+            types.add(ChessPiece.PieceType.KNIGHT);
+        }
+        else {types.add(null);}
 
-        //forward
-        ChessMove forward = new ChessMove(position, new ChessPosition(newRow, position.getColumn()), type);
-        if (board.getPiece(forward.getEndPosition())==null && inBounds(forward.getEndPosition())){
-            moves.add(forward);
+
+        for (ChessPiece.PieceType type : types) {
+            //forward
+            ChessMove forward = new ChessMove(position, new ChessPosition(newRow, position.getColumn()), type);
+            if (board.getPiece(forward.getEndPosition()) == null && inBounds(forward.getEndPosition())) {
+                moves.add(forward);
+            }
+
+            //attack
+            ChessMove attack1 = new ChessMove(position, new ChessPosition(newRow, position.getColumn() - 1), type);
+            if (board.getPiece(attack1.getEndPosition()) != null && board.getPiece(attack1.getEndPosition()).getTeamColor() != color && inBounds(attack1.getEndPosition())) {
+                moves.add(attack1);
+            }
+            ChessMove attack2 = new ChessMove(position, new ChessPosition(newRow, position.getColumn() + 1), type);
+            if (board.getPiece(attack2.getEndPosition()) != null && board.getPiece(attack2.getEndPosition()).getTeamColor() != color && inBounds(attack2.getEndPosition())) {
+                moves.add(attack2);
+            }
+        }
+        //first move
+        if (position.getRow()==2 && board.getPiece(position).getTeamColor()== ChessGame.TeamColor.WHITE){
+            moves.add(new ChessMove(position, new ChessPosition(4, position.getColumn()), null));
+        }
+        if (position.getRow()==7 && board.getPiece(position).getTeamColor()== ChessGame.TeamColor.BLACK){
+            moves.add(new ChessMove(position, new ChessPosition(5, position.getColumn()), null));
         }
 
-        //attack
-        ChessMove attack1 = new ChessMove(position, new ChessPosition(newRow, position.getColumn()-1), type);
-        if (board.getPiece(forward.getEndPosition())!=null && board.getPiece(forward.getEndPosition()).getTeamColor()!=color && inBounds(forward.getEndPosition())){
-            moves.add(attack1);
-        }
-        ChessMove attack2 = new ChessMove(position, new ChessPosition(newRow, position.getColumn()+1), type);
-        if (board.getPiece(forward.getEndPosition())!=null && board.getPiece(forward.getEndPosition()).getTeamColor()!=color && inBounds(forward.getEndPosition())){
-            moves.add(attack2);
-        }
     }
 }
