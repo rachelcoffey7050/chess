544f4cc9067528dac2b3d06ace6393d7d89a3b74
Rachel Coffey <rachelc7050@gmail.com>
2026-01-21 18:31:38 -0700

    hopefully fixed queen, knight, rook

--- shared/src/main/java/chess/movetypes/RookMove.java
+++ shared/src/main/java/chess/movetypes/RookMove.java
@@ -17,24 +17,36 @@ public class RookMove extends Moves{
             ChessPosition p1 = new ChessPosition(position.getRow(), position.getColumn() + i);
             if (tracker1 && inBounds(p1) && (board.getPiece(p1) == null || board.getPiece(p1).getTeamColor()!=color)) {
                 this.moves.add(new ChessMove(position, p1, null));
+                if (board.getPiece(p1).getTeamColor()!=color) {
+                    tracker1 = false;
+                }
             } else {
                 tracker1 = false;
             }
             ChessPosition p2 = new ChessPosition(position.getRow() - i, position.getColumn());
             if (tracker2 && inBounds(p2) && (board.getPiece(p2) == null || board.getPiece(p2).getTeamColor()!=color)) {
                 this.moves.add(new ChessMove(position, p2, null));
+                if (board.getPiece(p1).getTeamColor()!=color) {
+                    tracker2 = false;
+                }
             } else {
                 tracker2 = false;
             }
             ChessPosition p3 = new ChessPosition(position.getRow(), position.getColumn() - i);
             if (tracker3 && inBounds(p3) && (board.getPiece(p3) == null || board.getPiece(p3).getTeamColor()!=color)) {
                 this.moves.add(new ChessMove(position, p3, null));
+                if (board.getPiece(p1).getTeamColor()!=color) {
+                    tracker3 = false;
+                }
             } else {
                 tracker3 = false;
             }
             ChessPosition p4 = new ChessPosition(position.getRow() + i, position.getColumn());
             if (tracker4 && inBounds(p4) && (board.getPiece(p4) == null || board.getPiece(p4).getTeamColor()!=color)) {
                 this.moves.add(new ChessMove(position, p4, null));
+                if (board.getPiece(p1).getTeamColor()!=color) {
+                    tracker4 = false;
+                }
             } else {
                 tracker4 = false;
             }
