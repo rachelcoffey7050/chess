12fe6a1ef8f276103d9b8a2ca375540d5cd1dd10
Rachel Coffey <rachelc7050@gmail.com>
2026-02-28 12:43:19 -0700

    complete models

--- server/src/main/java/server/Server.java
+++ server/src/main/java/server/Server.java
@@ -1,6 +1,9 @@
 package server;
 
 import io.javalin.*;
+import io.javalin.Javalin;
+import io.javalin.http.Context;
+import com.google.gson.Gson;
 
 public class Server {
 
@@ -8,7 +11,6 @@ public class Server {
 
     public Server() {
         javalin = Javalin.create(config -> config.staticFiles.add("web"));
-
         // Register your endpoints and exception handlers here.
 
     }
---
+++ shared/src/main/java/model/AuthData.java
@@ -0,0 +1,3 @@
+package model;
+
+record AuthData(String username, String authToken) {}
