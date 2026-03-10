d6fa3553d9d4ccfee6fcdf66cfddec12a4fc8708
Rachel Coffey <rachelc7050@gmail.com>
2026-03-10 16:45:17 -0600

    sql gameDAO

--- server/src/main/java/dataaccess/GameDAO.java
+++ server/src/main/java/dataaccess/GameDAO.java
@@ -1,20 +1,21 @@
 package dataaccess;
 
 import model.GameData;
+import service.exceptions.ResponseException;
 
 import java.util.HashMap;
 import java.util.List;
 
 public interface GameDAO {
 
-    void deleteAll();
+    void deleteAll() throws ResponseException;
 
-    Integer addGame(GameData data) throws DataAccessException;
+    Integer addGame(GameData data) throws DataAccessException, ResponseException;
 
-    GameData findGame(Integer gameID) throws DataAccessException;
+    GameData findGame(Integer gameID) throws DataAccessException, ResponseException;
 
-    void updateGame(GameData gameData) throws DataAccessException;
+    void updateGame(GameData gameData) throws DataAccessException, ResponseException;
 
-    HashMap<Integer, GameData> getGames() throws DataAccessException;
+    HashMap<Integer, GameData> getGames() throws DataAccessException, ResponseException;
 
 }
---
+++ server/src/main/java/dataaccess/SQLGameDAO.java
@@ -0,0 +1,90 @@
+package dataaccess;
+
+import com.google.gson.Gson;
+import model.AuthData;
+import model.GameData;
+import service.exceptions.ResponseException;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.HashMap;
+
+import static dataaccess.DatabaseCreator.executeUpdate;
+
+public class SQLGameDAO implements GameDAO{
+
+    public SQLGameDAO() throws ResponseException, DataAccessException {
+        String[] createStatements = {
+                """
+            CREATE TABLE IF NOT EXISTS  games (
+              `id` int NOT NULL AUTO_INCREMENT,
+              `json` TEXT DEFAULT NULL,
+              PRIMARY KEY (`id`),
+            ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci
+            """
+        };
+        DatabaseCreator.configureDatabase(createStatements);
+    }
+
+    public Integer addGame(GameData game) throws DataAccessException, ResponseException {
+        var statement = "INSERT INTO games (id, json) VALUES (?, ?)";
+        String json = new Gson().toJson(game);
+        Integer id = executeUpdate(statement, json);
+        return id;
+    }
+
+    public HashMap<Integer, GameData> getGames() throws ResponseException {
+        var result = new HashMap<Integer, GameData>();
+        try (Connection conn = DatabaseManager.getConnection()) {
+            var statement = "SELECT id, json FROM pet";
+            try (PreparedStatement ps = conn.prepareStatement(statement)) {
+                try (ResultSet rs = ps.executeQuery()) {
+                    while (rs.next()) {
+                        GameData eachGame = readGame(rs);
+                        result.put(eachGame.gameID(), eachGame);
+                    }
+                }
+            }
+        } catch (Exception e) {
+            throw new ResponseException(ResponseException.Code.ServerError, String.format("Unable to read data: %s", e.getMessage()));
+        }
+        return result;
+    }
+
+    public GameData findGame(Integer gameID) throws ResponseException {
+        try (Connection conn = DatabaseManager.getConnection()) {
+            var statement = "SELECT id, json FROM games WHERE id=?";
+            try (PreparedStatement ps = conn.prepareStatement(statement)) {
+                ps.setInt(1, gameID);
+                try (ResultSet rs = ps.executeQuery()) {
+                    if (rs.next()) {
+                        return readGame(rs);
+                    }
+                }
+            }
+        } catch (Exception e) {
+            throw new ResponseException(ResponseException.Code.ServerError, String.format("Unable to read data: %s", e.getMessage()));
+        }
+        return null;
+    }
+
+    public void updateGame(GameData gameData) throws ResponseException {
+        var statement = "DELETE FROM games WHERE id=?";
+        executeUpdate(statement, gameData.gameID());
+        var statement2 = "INSERT INTO games (id, json) VALUES (?, ?)";
+        String json = new Gson().toJson(gameData);
+        Integer id = executeUpdate(statement2, json);
+    }
+
+    private GameData readGame(ResultSet rs) throws SQLException {
+        var json = rs.getString("json");
+        return new Gson().fromJson(json, GameData.class);
+    }
+
+    public void deleteAll() throws ResponseException {
+        var statement = "TRUNCATE games";
+        executeUpdate(statement);
+    }
+}
