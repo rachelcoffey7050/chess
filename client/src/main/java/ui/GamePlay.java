750d8ad4965f7a44931394bf3a1d4119882bca85
Rachel Coffey <rachelc7050@gmail.com>
2026-04-08 14:45:58 -0600

    game play ui outline

---
+++ client/src/main/java/ui/GamePlay.java
@@ -0,0 +1,64 @@
+package ui;
+
+import client.ServerFacade;
+
+import java.util.Scanner;
+
+public class GamePlay {
+
+    private final ServerFacade facade;
+    private final BoardPrinter printer;
+
+    public GamePlay(ServerFacade facade, BoardPrinter printer){
+        this.facade = facade;
+        this.printer = printer;
+    }
+
+    public void runGamePlay(){
+        Scanner sc = new Scanner(System.in);
+
+        if (!sc.hasNextInt()) {
+            System.out.println("Invalid input: Please type a number");
+            sc.nextLine();
+            return;
+        }
+
+        int n = sc.nextInt();
+
+        if (n == 1) {help();}
+        else if (n == 2) {redrawChessBoard();}
+        else if (n==3) {leave();}
+        else if (n == 4) {makeMove();}
+        else if (n == 5) {resign();}
+        else if (n == 6) {highlight();}
+        else {
+            System.out.println("Invalid Input");
+        }
+    }
+
+    private void help(){
+        System.out.println("Menu:\n1. Help\n2. Redraw Chess Board\n3. Leave\n4. Make Move\n5. Resign\n6. Highlight Legal Moves ");
+    }
+
+    private void leave(){
+        System.out.println("Leaving Chess Game");
+        // go back to post Login
+    }
+
+    private void redrawChessBoard(){
+        printer.printBoard();
+    }
+
+    private void makeMove(){
+        // make move with websocket?
+        printer.printBoard();
+    }
+     private void resign(){
+         System.out.println("Game Over: Forfeited");
+         // websocket ends game?
+     }
+
+     private void highlight(){
+        //get list of legal moves from chess game and print them out
+     }
+}
